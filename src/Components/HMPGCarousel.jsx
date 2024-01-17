65f52f46efba30c4a82b8d051e21f4fd223540fe
Oluwatimileyin Dada <oluwatimileyin962@gmail.com>
2024-01-17 11:30:34 +0100

    carousel homepage

--- src/Components/HMPGCarousel.jsx
+++ src/Components/HMPGCarousel.jsx
@@ -1,62 +1,44 @@
-import React, { useState, useEffect } from "react";
-import { CarouselBullet, CarouselBulletWhite } from "./svgs";
 import {Link} from 'react-router-dom'
 
-const HMPGCarousel = ({ slides }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+import React from 'react'
+import 'react-slideshow-image/dist/styles.css'
+import {Fade, Zoom, Slide} from 'react-slideshow-image'
 
+const HMPGCarousel = ({slides}) => {
 
-  const styles = {
-    width: "100%",
-    backgroundImage: `url(${slides[currentIndex].url})`,
-    height: "100%",
-    borderRadius: "10px",
-    backgroundPosition: "center",
-    backgroundSize: "cover"
-  }
+    const divStyle = {
+        // height: '600px',
+        // color: 'white',
+        // textAlign: 'center',
+        // display: 'flex',
+        // flexDirection:'column',
+        // alignItems: 'center',
+        // justify
 
-  const goToPrevious = () =>{
-    const isFirst = currentIndex === 0;
-    const newIndex = isFirst ? slides.length - 1 : currentIndex - 1
-    setCurrentIndex(newIndex)
-
-  }
-
-  const goToNext = () => {
-    const isLast = currentIndex === slides.length - 1;
-    const newIndex = isLast ? 0 : currentIndex + 1
-    setCurrentIndex(newIndex)
-
-  }
-
+    }
 
   return (
-    <div className="HMPG">
-        <div style={styles} className="carousel-div homepage">
-           <div>
-                <div className="maintenance">{slides[currentIndex].title}</div>
-                <div className="service">{slides[currentIndex].contentA}</div>
-                <div className="nnaezi">
-                    {slides[currentIndex].contentB}
-                </div>
-                <div className="text-center">
-                    <Link to="/listing" type="button" className="btn btn-lg btn-danger">
-                    Inventory
-                    </Link>
-                </div>
-           </div>
-        </div>
-        <div className="bullet">
-        {slides.map((slide, index) => (
-                index === currentIndex ?
-                (<div key={index} className="carousel-bullet" onClick = {() => setCurrentIndex(index)}><CarouselBulletWhite /></div>)
+    <>
+      <Slide>
+        {
+          slides.map((slide, index) => (
+            <div key={index} style={{...divStyle, backgroundImage: `url(${slide.url})`}} className='slide'>
+                    <div className="maintenance">{slide.title}</div>
+                    <div className="service">{slide.contentA}</div>
+                    <div className="nnaezi">
+                        {slide.contentB}
+                    </div>
+                    <div className="text-center">
+                        <Link to="/listing" type="button" className="btn btn-lg btn-danger">
+                        Inventory
+                        </Link>
+                    </div>
+       </div>
+          ))
+        }
+      </Slide>
+    </>
+  )
+}
 
-                :
-                (<div key={index} className="carousel-bullet" onClick = {() => setCurrentIndex(index)}><CarouselBullet /></div>)
-            ))}
-        </div>
-    </div>
-  );
-};
-
-export default HMPGCarousel;
+export default HMPGCarousel
