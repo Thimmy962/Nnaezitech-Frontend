bd6644a958a401f24d6e496c65b066a3f1cea26a
Dada Oluwatimileyin <107270439+Thimmy962@users.noreply.github.com>
2024-01-28 18:00:45 +0100

    Update Contact.jsx added the main map

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -48,11 +48,11 @@ const Contact = () => {
               </div>
           </div>
         </div>
-
-        <div id='map'>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d990.9239607460398!2d3.336597669497384!3d6.560024931914192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8e07116281e3%3A0x7e0473febaaba8be!2s11%20Rafiu%20Cres%2C%20Mafoluku%20Oshodi%20102214%2C%20Lagos!5e0!3m2!1sen!2sng!4v1701165010036!5m2!1sen!2sng" 
-        style={{width: "600px", height: "450px"}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-        </div>
+      
+      <div id='map'>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d247.7354442044383!2d3.3334688!3d6.5510609!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8e0c0108723f%3A0x482c176a4e75052!2sAirport%20Cl%2C%20Orile%20Oshodi%2C%20Mafoluku%20102214%2C%20Lagos!5e0!3m2!1sen!2sng!4v1706460973162!5m2!1sen!2sng" 
+          style={{width:"600px", height:"450px", border:'0'}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
         
         <div ref={(ref) => (getinTouch.current[0] = ref)}>
             <GetInTouch />
