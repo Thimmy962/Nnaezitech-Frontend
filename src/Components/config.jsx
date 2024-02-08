c5aa2fd707322f65696c4349b0e2848f224983b2
Oluwatimileyin Dada <oluwatimileyin962@gmail.com>
2024-02-08 03:52:45 +0100

    adjusted config page

--- src/Components/config.jsx
+++ src/Components/config.jsx
@@ -1,8 +1,8 @@
-//  const API_BASE_URL = "https://thimmy962.pythonanywhere.com/";
-//  const MEDIA_BASE_URL = "https://thimmy962.pythonanywhere.com"
-//  export { API_BASE_URL, MEDIA_BASE_URL };
-
-
-const API_BASE_URL = "http://localhost:8000/";
-const MEDIA_BASE_URL = "http://localhost:8000/";
+const API_BASE_URL = "https://thimmy962.pythonanywhere.com/";
+const MEDIA_BASE_URL = "https://thimmy962.pythonanywhere.com"
 export { API_BASE_URL, MEDIA_BASE_URL };
+
+
+// const API_BASE_URL = "http://localhost:8000/";
+// const MEDIA_BASE_URL = "http://localhost:8000/";
+// export { API_BASE_URL, MEDIA_BASE_URL };
